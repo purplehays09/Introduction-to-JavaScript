99a1a870c009268daf97f34917392ab8707b4af3
purplehays09 <purplehays527@gmail.com>
2020-07-14 14:36:45 -0600

    I am done with the MVP. I tried to push myself to try harder versions of things or get input or confirmations on things, and it seems to be working from what I can tell.

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function mult(a,b){
 
 function getAge(){
     let age = prompt("What is your age?");
-    if (typeof age !== "number"){
+    if (typeof Number(age) !== "number"){
         while (typeof age !== 'number'){
             age = prompt("Sorry, that wasn't a number, please try again!\nWhat is your age?");
         }
@@ -65,6 +65,49 @@ console.log(dogAge);
 //Dog feeder 
 //takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
 
+function getDog(){
+    let name = prompt('What is your dogs name?')
+    let weight = Number(prompt('What is your dogs weight in pounds?'))
+    let age = getAge()
+
+    let dog = {
+        Name:name,
+        Weight:weight,
+        Age:age
+    }
+    return dog;
+}
+
+function months(years){
+    let numMonths = Math.floor(years * 12)
+}
+
+function feed(dog){
+    let pounds = 0;
+
+    if (dog.Age < 1){
+        let numMonths = months(dog.Age);
+        if (numMonths >= 7){
+            pounds = .04 * dog.Weight;
+        }else if(numMonths >= 4){
+            pounds = .05 * dog.Weight;
+        }else if(numMonths >= 2){
+            pounds = .1 * dog.Weight;
+        }else{
+            pounds = "we just don't know"
+        }
+    }else if(dog.Weight > 15){
+        pounds = .02 * dog.Weight;
+    }else if(dog.Weight >= 11){
+        pounds = .03 * dog.Weight;
+    }else if(dog.Weight >= 6){
+        pounds = .04 * dog.Weight;
+    }else{
+        pounds = .05 * dog.Weight;
+    }
+    return pounds;
+}
+
 //feeding requirements
 // adult dogs at least 1 year 
 // up to 5 lbs - 5% of their body weight
@@ -79,6 +122,39 @@ console.log(dogAge);
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
+let dogFood = feed(getDog())
+
+// simple version
+
+function feed(weight, age){
+    let pounds = 0;
+
+    if (years < 1){
+        let numMonths = months(age);
+        if (numMonths >= 7){
+            pounds = .04 * weight;
+        }else if(numMonths >= 4){
+            pounds = .05 * weight;
+        }else if(numMonths >= 2){
+            pounds = .1 * weight;
+        }else{
+            pounds = "we just don't know"
+        }
+    }else if(weight > 15){
+        pounds = .02 * weight;
+    }else if(weight >= 11){
+        pounds = .03 * weight;
+    }else if(weight >= 6){
+        pounds = .04 * weight;
+    }else{
+        pounds = .05 * weight;
+    }
+    return pounds;
+}
+
+feed(15,1)
+
+
 
 
 
@@ -90,19 +166,72 @@ console.log(dogAge);
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
+function pick(){
+    let selection = prompt("Do you pick Rock, Paper, or Scissors?");
+    if (selection !== "Rock" || "Paper" || "Scissors"){
+        while (selection !== "Rock" || "Paper" || "Scissors"){ 
+            selection = prompt("No for reals, you have to ype exactly 'Rock' 'Paper' or 'Scissors'.\nSelect again!");
+        }
+    return selection;
+    }
+}
+
+function contest(human){
+    let enemyNum = Math.random();
+    let enemy;
+    let result;
+    if (enemyNum > (2/3)){
+        enemy = "Rock";
+    }else if(enemyNum > (1/3)){
+        enemy = "Paper";
+    }else{
+        enemy = "Scissors"
+    }
+
+    if (human === "Rock"){
+        if (enemy === 'Rock'){
+            console.log("You tie");
+        }else if(enemy === "Paper"){
+            console.log('You lose');
+        }else{
+            console.log('You WIN!');
+        }
+    }else if(human === "Paper"){
+        if (enemy === 'Rock'){
+            console.log("You WIN!");
+        }else if(enemy === "Paper"){
+            console.log('You tie');
+        }else{
+            console.log('You lose');
+        }
+    }else{
+        if (enemy === 'Rock'){
+            console.log("You lose");
+        }else if(enemy === "Paper"){
+            console.log('You WIN!');
+        }else{
+            console.log('You tie');
+        }
+    }
+}
+
+console.log(contest(pick()))
   
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
-
+function kmToMiles(num){
+    return num * 1.60934;
+}
 
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+function feetToCm(num){
+    return num * 30.48;
+}
 
 
 
@@ -112,6 +241,15 @@ console.log(dogAge);
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
+function annoyingSong(num){
+    for (i = num; i > 0; i--){
+        let current = num;
+        console.log(`${current} bottles of beer on the wall\n${current} bottles of beer`);
+        current --;
+        console.log(`take one down, pass it around\n${current} bottles of beer on the wall.`);
+    }
+}
+annoyingSong(99);
 
 
 
@@ -124,9 +262,35 @@ console.log(dogAge);
 //70s should be Cs 
 //60s should be D 
 //and anything below 60 should be F
+//lookup include 
   
+function getGrades(){
+    let unfinished = true;
+    let grades = [];
 
-  
+    while(unfinished === true){
+        unfinished = confirm("Do you have more grades to enter?");
+        grades.push(Number(prompt("What is the number grade?")));
+    }
+
+}
+
+function teacherAssistant(grades){
+    let letterGrades = [];
+    for (grade in grades[grade]){
+        if (grade > 90){
+            letterGrades.push('A');
+        }else if(grade > 80) {
+            letterGrades.push('B');
+        }else if(grade > 70){
+            letterGrades.push('C');
+        }else if(grade > 60){
+            letterGrades.push('D');
+        }else{
+            letterGrades.push('F');
+        }
+    }
+}  
   
 
 /************************************************************** Stretch **************************************************************/
